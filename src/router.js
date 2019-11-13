ed4bbb52c16942d25db7a46d918ca070b4428662
代帅卿 <daishuaiqing@163.com>
2019-11-13 12:05:20 +0800

    英文版本修改完成

--- src/router.js
+++ src/router.js
@@ -12,7 +12,7 @@ export default new Router({
       name: 'page1',
       meta: {
         index: 0,
-        title: '博华广场'
+        title: 'ONE MUSEUM PLACE'
       },
       component: () => import('./views/page1.vue')
     },
@@ -21,7 +21,7 @@ export default new Router({
       name: 'page2',
       meta: {
         index: 1,
-        title: '博华广场'
+        title: 'ONE MUSEUM PLACE'
       },
       component: () => import('./views/page2.vue')
     },
@@ -30,7 +30,7 @@ export default new Router({
       name: 'page3',
       meta: {
         index: 2,
-        title: '地铁线路图'
+        title: 'Metro Map'
       },
       component: () => import('./views/page3.vue')
     },
@@ -39,7 +39,7 @@ export default new Router({
       name: 'page4',
       meta: {
         index: 3,
-        title: '地块图'
+        title: 'Plot Plan'
       },
       component: () => import('./views/page4.vue')
     },
@@ -48,7 +48,7 @@ export default new Router({
       name: 'page5',
       meta: {
         index: 4,
-        title: '实景图'
+        title: 'Real Picture'
       },
       component: () => import('./views/page5.vue')
     },
@@ -57,7 +57,7 @@ export default new Router({
       name: 'page6',
       meta: {
         index: 5,
-        title: '楼层图'
+        title: 'Floor Plan'
       },
       component: () => import('./views/page6.vue')
     },
@@ -66,7 +66,7 @@ export default new Router({
       name: 'page7',
       meta: {
         index: 6,
-        title: '租赁服务咨询'
+        title: 'Leasing Service Consultation'
       },
       component: () => import('./views/page7.vue')
     },
@@ -75,7 +75,7 @@ export default new Router({
       name: 'page8',
       meta: {
         index: 7,
-        title: '办公楼'
+        title: 'Floor layout'
       },
       component: () => import('./views/page8.vue')
     }
